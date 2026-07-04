5f816ada1f04672ed6519adc7ba5248b3267f8d3
Luciano Joaquin Maciel <luchomaciel9@gmail.com>
2026-07-04 12:46:41 -0300

    Fix SVG path attribute in MaterialsPage upload button

--- src/pages/MaterialsPage.tsx
+++ src/pages/MaterialsPage.tsx
@@ -134,7 +134,7 @@ function MaterialsPage() {
                   strokeLinecap="round"
                   strokeLinejoin="round"
                   strokeWidth={2}
-                  d="M4 16v1a3 3 0 003 3h10a3 3 003-3v-1m-4-8l-4-4m0 0L8 8m4-4v12"
+                  d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-8l-4-4m0 0L8 8m4-4v12"
                 />
               </svg>
               Subir archivos
